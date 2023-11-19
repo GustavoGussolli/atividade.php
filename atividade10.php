e49bd3a4df7c3bd9ba7bcd493f86d9a4517dc44d
GustavoGussolli <134729255+GustavoGussolli@users.noreply.github.com>
2023-11-18 21:56:29 -0300

    Update atividade10.php

--- atividade10.php
+++ atividade10.php
@@ -1,1 +1,21 @@
+<?php
 
+function removerCaractere(&$str, &$caractere) {
+    $count = 0;
+    // Substitui todas as ocorrências do caractere na string por uma string vazia
+    $str = str_replace($caractere, '', $str, $count);
+    // Retorna o número de vezes que o caractere foi removido
+    return $count;
+}
+
+// Exemplo de uso:
+$minhaString = "banana";
+$meuCaractere = "a";
+
+echo "String original: $minhaString\n";
+echo "Caractere a ser removido: $meuCaractere\n";
+
+$ocorrenciasRemovidas = removerCaractere($minhaString, $meuCaractere);
+
+echo "String modificada: $minhaString\n";
+echo "Número de vezes que o caractere foi removido: $ocorrenciasRemovidas\n";
